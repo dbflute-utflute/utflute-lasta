c27fbbd85d7462bf84eada2aba396d0bd319b272
jflute <dbflute@gmail.com>
2015-08-02 15:52:17 +0900

    fix switching of app.xml when web

--- utflute-lastaflute/src/main/java/org/dbflute/utflute/lastaflute/WebContainerTestCase.java
+++ utflute-lastaflute/src/main/java/org/dbflute/utflute/lastaflute/WebContainerTestCase.java
@@ -169,6 +169,7 @@ public abstract class WebContainerTestCase extends ContainerTestCase {
     }
 
     protected void xdoInitializeContainerAsWeb(String configFile) {
+        SingletonLaContainerFactory.setConfigPath(configFile);
         final ServletConfig servletConfig = xprepareMockServletConfig(configFile);
         final LastaFilter filter = xcreateLastaFilter();
         try {
