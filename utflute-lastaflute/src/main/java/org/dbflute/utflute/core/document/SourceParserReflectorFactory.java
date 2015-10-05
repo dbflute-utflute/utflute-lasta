2bfe12097a266ae36fd2d12c606dde417b3ff1dc
jflute <dbflute@gmail.com>
2015-10-05 21:17:02 +0900

    add logging

--- utflute-lastaflute/src/main/java/org/dbflute/utflute/core/document/SourceParserReflectorFactory.java
+++ utflute-lastaflute/src/main/java/org/dbflute/utflute/core/document/SourceParserReflectorFactory.java
@@ -20,6 +20,8 @@ import java.util.List;
 import org.dbflute.optional.OptionalThing;
 import org.dbflute.util.DfReflectionUtil;
 import org.dbflute.util.DfReflectionUtil.ReflectionFailureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author p1us2er0
@@ -31,20 +33,23 @@ public class SourceParserReflectorFactory {
     //                                                                          Definition
     //                                                                          ==========
     private static final String JAVA_PARSER_CLASS_NAME = "com.github.javaparser.JavaParser";
+    private static final Logger _log = LoggerFactory.getLogger(SourceParserReflectorFactory.class);
 
     // ===================================================================================
     //                                                                           Reflector
     //                                                                           =========
     public OptionalThing<SourceParserReflector> reflector(List<String> srcDirList) { // empty allowed if not found
+        final String className = JAVA_PARSER_CLASS_NAME;
         SourceParserReflector sourceParserReflector = null;
         try {
-            DfReflectionUtil.forName(JAVA_PARSER_CLASS_NAME);
+            DfReflectionUtil.forName(className);
+            _log.debug("...Loading java parser for document: {}", className);
             sourceParserReflector = new JavaparserSourceParserReflector(srcDirList);
-        } catch (ReflectionFailureException e) {
+        } catch (ReflectionFailureException ignored) {
             sourceParserReflector = null;
         }
         return OptionalThing.ofNullable(sourceParserReflector, () -> {
-            throw new IllegalStateException("Not found the java parser: " + JAVA_PARSER_CLASS_NAME);
+            throw new IllegalStateException("Not found the java parser: " + className);
         });
     }
 }
