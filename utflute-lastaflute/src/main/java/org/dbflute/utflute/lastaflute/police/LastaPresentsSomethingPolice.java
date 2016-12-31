63e00e6b14cba80e4aa5858084af1afe39e80769
jflute <dbflute@gmail.com>
2016-12-31 21:36:01 +0900

    form immutable police

--- utflute-lastaflute/src/main/java/org/dbflute/utflute/lastaflute/police/LastaPresentsSomethingPolice.java
+++ utflute-lastaflute/src/main/java/org/dbflute/utflute/lastaflute/police/LastaPresentsSomethingPolice.java
@@ -16,8 +16,12 @@
 package org.dbflute.utflute.lastaflute.police;
 
 import java.io.File;
+import java.lang.reflect.Modifier;
 
+import org.dbflute.helper.message.ExceptionMessageBuilder;
+import org.dbflute.utflute.core.filesystem.FilesystemPlayer;
 import org.dbflute.utflute.core.policestory.javaclass.PoliceStoryJavaClassHandler;
+import org.dbflute.util.Srl;
 
 /**
  * @author jflute
@@ -26,9 +30,100 @@ import org.dbflute.utflute.core.policestory.javaclass.PoliceStoryJavaClassHandle
 public class LastaPresentsSomethingPolice implements PoliceStoryJavaClassHandler {
 
     // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    protected boolean _formImmutable;
+
+    // ===================================================================================
     //                                                                              Handle
     //                                                                              ======
     public void handle(File srcFile, Class<?> clazz) {
-        // nothing for now (for future)
+        handleFormImmutable(srcFile, clazz);
+    }
+
+    // ===================================================================================
+    //                                                                      Form Immutable
+    //                                                                      ==============
+    protected void handleFormImmutable(File srcFile, Class<?> clazz) {
+        if (clazz.isInterface() || Modifier.isAbstract(clazz.getModifiers())) { // e.g. BaseAction
+            return;
+        }
+        if (isFormImmutableTargetClass(clazz)) {
+            new FilesystemPlayer().readLine(srcFile, "UTF-8", line -> {
+                if (_formImmutable) {
+                    checkFormImmutableLine(clazz, line, "Form", "form."); // option
+                }
+                checkFormImmutableLine(clazz, line, "Body", "body."); // as default
+            });
+        }
+    }
+
+    protected boolean isFormImmutableTargetClass(Class<?> clazz) {
+        final String name = clazz.getName();
+        return name.contains(getWebPackageKeyword()) && (name.endsWith(getActionSuffix()) || name.endsWith(getAssistSuffix()));
+    }
+
+    protected void checkFormImmutableLine(Class<?> clazz, String line, String title, String formPrefix) {
+        if (line.contains(formPrefix)) { // simple determination for performance
+            final String ltrimmedLine = Srl.ltrim(line);
+            if (ltrimmedLine.startsWith(formPrefix) && ltrimmedLine.length() != line.length()) {
+                final String formRear = Srl.substringFirstRear(line, formPrefix);
+                if (formRear.contains(" = ")) { // not complete for now
+                    throwFormImmutableBreakException(clazz, title, ltrimmedLine);
+                }
+            }
+        }
+    }
+
+    protected void throwFormImmutableBreakException(Class<?> clazz, String title, final String ltrimmedLine) {
+        final ExceptionMessageBuilder br = new ExceptionMessageBuilder();
+        br.addNotice(title + " should be immutable but re-assignment was found.");
+        br.addItem("Advice");
+        br.addElement("Form and Body should be read-only object.");
+        br.addElement("For example:");
+        br.addElement("  (x):");
+        br.addElement("    form.memberName = \"sea\"; // *Bad");
+        br.addElement("    body.memberAccount = \"land\"; // *Bad");
+        br.addElement("  (o):");
+        br.addElement("    selectMember(form.memberName); // Good");
+        br.addElement("    selectMember(body.memberAccount); // Good");
+        br.addItem("Target Class");
+        br.addElement(clazz);
+        br.addItem("Wrong Statement");
+        br.addElement(ltrimmedLine);
+        final String msg = br.buildExceptionMessage();
+        throw new FormImmutableBrokenException(msg);
+    }
+
+    public static class FormImmutableBrokenException extends RuntimeException {
+
+        private static final long serialVersionUID = 1L;
+
+        public FormImmutableBrokenException(String msg) {
+            super(msg);
+        }
+    }
+
+    // ===================================================================================
+    //                                                                              Naming
+    //                                                                              ======
+    protected String getWebPackageKeyword() {
+        return ".app.web.";
+    }
+
+    protected String getActionSuffix() {
+        return "Action";
+    }
+
+    protected String getAssistSuffix() {
+        return "Assist";
+    }
+
+    // ===================================================================================
+    //                                                                              Option
+    //                                                                              ======
+    public LastaPresentsSomethingPolice formImmutable() {
+        _formImmutable = true;
+        return this;
     }
 }
